2d456c8e7197d56c44d27f3ff5dae059deccc485
Willy Lee <mnak0209@gmail.com>
2020-02-13 23:18:41 +0800

    add read file

--- product.py
+++ product.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品名稱,商品價格' in line:
+			continue # 遇到商品跟價格就跳過那一行
+		name, price = line.strip().split(',') #strip是去除\n的功能
+		products.append([name, price])
+print(products)
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -12,9 +22,11 @@ while True:
 	products.append([name, price])#等於上面幾行
 print(products)
 
+#印出所有購買紀錄
 for p in products: #把一個一個清單的東西拿出來
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open ('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品名稱,商品價格\n')
 	for p in products:
